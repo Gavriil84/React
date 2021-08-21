8abe997cbce72f4f2a0a4bebc21400f0f83045d7
Gavriil84 <79692322+Gavriil84@users.noreply.github.com>
2021-08-21 12:03:34 -0700

    spread syntax

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import "./styles.css";
+//  スプレッド構文 ...
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(...arr1)
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(...arr1)
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+console.log(arr6);
+
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
