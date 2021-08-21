3f61e1674e4bd2357fd62e2e66dc1cfde18bfdc4
Gavriil84 <79692322+Gavriil84@users.noreply.github.com>
2021-08-21 12:52:01 -0700

    ternary operator

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ console.log(arr6);
 const arr7 = [...arr4, ...arr5];
 console.log(arr7);
 
-// map, filterを使った配列の処理
+map, filterを使った配列の処理;
 
 const nameArr = ["田中", "山田", "下田"];
 for (let i = 0; i < nameArr.length; i++) {
@@ -55,3 +55,44 @@ const newNameArr = nameArr.map((name) => {
 });
 
 console.log(newNameArr);
+
+// 三項演算子
+// ある条件　？　条件がtrueの時 ：　条件がfalseの時
+
+const val1 = 1 < 0 ? "trueです" : "falseです";
+console.log(val1);
+
+const num = 1300;
+
+const formattedNum =
+  typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
+};
+console.log(checkSum(50, 50));
+
+// 論理演算子の本当の意味を知ろう
+
+const flag1 = true;
+const flag2 = true;
+
+if (flag1 || flag2) {
+  console.log("1か2はtrueになります");
+}
+
+if (flag1 && flag2) {
+  console.log("1も2はtrueになります");
+}
+
+// ||　は左側がfalseなら右側を返す
+// nullはfalse判定されるから文字列が返される
+const num = null;
+const fee = num || "金額未定です";
+console.log(fee);
+
+// &&　は左側がtrueなら右側を返す
+const num2 = null;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
