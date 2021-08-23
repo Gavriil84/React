ca62112a1d0505c2862874a08cdf92742a30c1c6
Gavriil84 <79692322+Gavriil84@users.noreply.github.com>
2021-08-23 16:28:15 -0700

    append todo list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // divタグを生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divタグにアペンド
+  div.appendChild(li);
+
+  // uncompleted list add
+  document.getElementById("uncompleted-list").appendChild(div);
+};
+
+document.getElementById("add-button").addEventListener("click", (e) => {
+  onClickAdd();
+});
