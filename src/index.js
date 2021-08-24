a1d736646ba6186b58487f050f2e42347958ee6f
Gavriil84 <79692322+Gavriil84@users.noreply.github.com>
2021-08-23 17:08:05 -0700

    add todo add and delete  function

--- src/index.js
+++ src/index.js
@@ -13,11 +13,36 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  //button生成
+
+  // complete btn
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "Complete";
+  completeButton.addEventListener("click", (e) => {
+    //押された完了ボタンの親タグ（div）を削除
+    deleteFromUncompletedList(deleteButton.parentNode);
+  });
+
+  // delete btn
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.addEventListener("click", (e) => {
+    //押された削除ボタンの親タグ（div）を削除
+    deleteFromUncompletedList(deleteButton.parentNode);
+  });
+
   // divタグにアペンド
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   // uncompleted list add
   document.getElementById("uncompleted-list").appendChild(div);
+};
+
+//未完了リストから指定の要素を削除
+const deleteFromUncompletedList = (target) => {
+  document.getElementById("uncompleted-list").removeChild(target);
 };
 
 document.getElementById("add-button").addEventListener("click", (e) => {
